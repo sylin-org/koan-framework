2b01eaa24af15377bc39307b43ba91b202bc21fe
Leo Botinelly <leonardo.botinelly@gmail.com>
2025-09-13 15:10:46 -0400

    Revert "entity enricher pipeline."
    This reverts commit 82e8461142feb45137c5b629531af36358c15bc9.

--- src/Sora.Data.Core/Relationships/IRelationshipMetadata.cs
+++ src/Sora.Data.Core/Relationships/IRelationshipMetadata.cs
@@ -5,7 +5,6 @@ namespace Sora.Data.Core.Relationships
 {
     public interface IRelationshipMetadata
     {
-    IReadOnlyList<(string PropertyName, Type ParentType)> GetParentRelationships(Type entityType);
-    IReadOnlyList<(string PropertyName, Type ChildType)> GetChildRelationships(Type entityType);
+        IReadOnlyList<(string PropertyName, Type ParentType)> GetParentRelationships(Type entityType);
     }
 }
--- src/Sora.Data.Core/Relationships/RelationshipMetadataService.cs
+++ src/Sora.Data.Core/Relationships/RelationshipMetadataService.cs
@@ -7,60 +7,14 @@ namespace Sora.Data.Core.Relationships
 {
     public class RelationshipMetadataService : IRelationshipMetadata
     {
-        private static readonly Dictionary<Type, List<(string PropertyName, Type ParentType)>> _parentGraph;
-        private static readonly Dictionary<Type, List<(string PropertyName, Type ChildType)>> _childGraph;
-
-        static RelationshipMetadataService()
-        {
-            _parentGraph = new();
-            _childGraph = new();
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var allTypes = assemblies.SelectMany(a => {
-                try { return a.GetTypes(); } catch { return Array.Empty<Type>(); }
-            }).ToList();
-
-            // Build parent graph
-            foreach (var t in allTypes)
-            {
-                var parentProps = t.GetProperties()
-                    .Select(p => (Property: p, Attr: p.GetCustomAttribute<ParentAttribute>()))
-                    .Where(x => x.Attr != null)
-                    .Select(x => (x.Property.Name, x.Attr.ParentType))
-                    .ToList();
-                if (parentProps.Count > 0)
-                    _parentGraph[t] = parentProps;
-            }
-
-            // Build child graph by scanning for [Parent] attributes referencing each type
-            foreach (var t in allTypes)
-            {
-                foreach (var other in allTypes)
-                {
-                    foreach (var prop in other.GetProperties())
-                    {
-                        var parentAttr = prop.GetCustomAttribute<ParentAttribute>();
-                        if (parentAttr != null && parentAttr.ParentType == t)
-                        {
-                            if (!_childGraph.TryGetValue(t, out var list))
-                            {
-                                list = new List<(string PropertyName, Type ChildType)>();
-                                _childGraph[t] = list;
-                            }
-                            list.Add((prop.Name, other));
-                        }
-                    }
-                }
-            }
-        }
-
         public IReadOnlyList<(string PropertyName, Type ParentType)> GetParentRelationships(Type entityType)
         {
-            return _parentGraph.TryGetValue(entityType, out var rels) ? rels : Array.Empty<(string, Type)>();
-        }
-
-        public IReadOnlyList<(string PropertyName, Type ChildType)> GetChildRelationships(Type entityType)
-        {
-            return _childGraph.TryGetValue(entityType, out var rels) ? rels : Array.Empty<(string, Type)>();
+            var parentProps = entityType.GetProperties()
+                .Select(p => (Property: p, Attr: p.GetCustomAttribute<ParentAttribute>()))
+                .Where(x => x.Attr != null)
+                .Select(x => (x.Property.Name, x.Attr.ParentType))
+                .ToList();
+            return parentProps;
         }
     }
 }
