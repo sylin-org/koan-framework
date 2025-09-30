a40c508d2150b419273eff45fe205bcf861ded24
Leo Botinelly <leonardo.botinelly@gmail.com>
2025-09-29 21:05:14 -0400

    Update Member.cs

--- samples/guides/g1c1.GardenCoop/Models/Member.cs
+++ samples/guides/g1c1.GardenCoop/Models/Member.cs
@@ -4,10 +4,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace g1c1.GardenCoop.Models;
 
-[DataAdapter("sqlite")]
 public class Member : Entity<Member>
 {
     [Required]
-    [MaxLength(64)]
     public string DisplayName { get; set; } = string.Empty;
 }
