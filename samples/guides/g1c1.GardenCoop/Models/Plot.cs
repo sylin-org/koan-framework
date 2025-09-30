99a235ec9d5eae1bb3dd3f1749bdb0a012a31e04
Leo Botinelly <leonardo.botinelly@gmail.com>
2025-09-29 21:05:10 -0400

    Update Plot.cs

--- samples/guides/g1c1.GardenCoop/Models/Plot.cs
+++ samples/guides/g1c1.GardenCoop/Models/Plot.cs
@@ -5,11 +5,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace g1c1.GardenCoop.Models;
 
-[DataAdapter("sqlite")]
 public class Plot : Entity<Plot>
 {
     [Required]
-    [MaxLength(64)]
     public string Name { get; set; } = string.Empty;
 
     [Parent(typeof(Member))]
